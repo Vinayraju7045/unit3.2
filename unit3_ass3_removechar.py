b665bed74402a2f8282baab60615a57f9b3240f1
Vinayraju7045 <67084004+Vinayraju7045@users.noreply.github.com>
2020-06-27 10:40:08 +0530

    Add files via upload

---
+++ unit3_ass3_removechar.py
@@ -0,0 +1,55 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 2,
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "enter string python class\n",
+      "index 6\n",
+      "string after deletion  pythonclass\n"
+     ]
+    }
+   ],
+   "source": [
+    "name=input(\"enter string \")\n",
+    "n=int(input(\"index \"))\n",
+    "first_part = name[:n] \n",
+    "last_part = name[n+1:]\n",
+    "print(\"string after deletion \",first_part + last_part)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.8.3"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 4
+}
